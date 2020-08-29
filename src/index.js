491fa162e9c291c8b4fd72a4e9a363887d049c3c
Brandon K. Waltz <bkwaltz@colby.edu>
2020-08-29 16:35:52 -0400

    adds progressive image loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-else-return */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ProgressiveImage from 'react-progressive-image';
 
 export default class ColbyImage extends Component {
     static propTypes = {
@@ -17,29 +19,86 @@ export default class ColbyImage extends Component {
 
         /** The image source for the lightbox */
         lightboxSrc: PropTypes.string,
+
+        /** Whether to lazy load the image or not */
+        lazyLoad: PropTypes.bool,
     };
 
     static defaultProps = {
         useLightbox: true,
         lightboxGallery: '',
         lightboxSrc: '',
+        lazyLoad: false,
     };
 
     render() {
-        if (!this.props.useLightbox) {
-            return <img src={this.props.src.main} className="img-fluid" alt={this.props.altText} />;
+        let renderElement;
+
+        if (this.props.lazyLoad && this.props.useLightbox) {
+            renderElement = (
+                <a
+                    data-fslightbox={
+                        this.props.lightboxGallery ? this.props.lightboxGallery : 'gallery'
+                    }
+                    href={this.props.lightboxSrc}
+                    data-type="image"
+                >
+                    <ProgressiveImage
+                        src={this.props.src.main}
+                        placeholder={this.props.src.thumbnail}
+                    >
+                        {(src, loading) => (
+                            <img
+                                style={{
+                                    ...{ filter: 'none' },
+                                    width: '100%',
+                                    filter: loading ? 'blur(3px)' : 'none',
+                                    transition: 'filter 100ms ease-in',
+                                }}
+                                src={src}
+                                className="img-fluid"
+                                alt={this.props.altText}
+                            />
+                        )}
+                    </ProgressiveImage>
+                </a>
+            );
+        } else if (this.props.lazyLoad && !this.props.useLightbox) {
+            renderElement = (
+                <ProgressiveImage src={this.props.src.main} placeholder={this.props.src.thumbnail}>
+                    {(src, loading) => (
+                        <img
+                            style={{
+                                ...{ filter: 'none' },
+                                width: '100%',
+                                filter: loading ? 'blur(3px)' : 'none',
+                                transition: 'filter 100ms ease-in',
+                            }}
+                            src={src}
+                            className="img-fluid"
+                            alt={this.props.altText}
+                        />
+                    )}
+                </ProgressiveImage>
+            );
+        } else if (!this.props.lazyLoad && this.props.useLightbox) {
+            renderElement = (
+                <a
+                    data-fslightbox={
+                        this.props.lightboxGallery ? this.props.lightboxGallery : 'gallery'
+                    }
+                    href={this.props.lightboxSrc}
+                    data-type="image"
+                >
+                    <img src={this.props.src.main} className="img-fluid" alt={this.props.altText} />
+                </a>
+            );
+        } else {
+            renderElement = (
+                <img src={this.props.src.main} className="img-fluid" alt={this.props.altText} />
+            );
         }
 
-        return (
-            <a
-                data-fslightbox={
-                    this.props.lightboxGallery ? this.props.lightboxGallery : 'gallery'
-                }
-                href={this.props.lightboxSrc}
-                data-type="image"
-            >
-                <img src={this.props.src.main} className="img-fluid" alt={this.props.altText} />
-            </a>
-        );
+        return renderElement;
     }
 }
